5104b8e01726d72c459a2ef8979165660e594f92
Daniel Kux <danielkux@gmail.com>
2018-09-01 23:28:04 -0300

    DEvolucion del clima tomando el promedio de horas

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -48,7 +48,7 @@ var openweathermap = {
 
     getData: async function executeParallelAsyncTasks () {
         const [ forecast ] = await Promise.all([ this.getFiveDaysForecast() ]);
-        return this.filterForecast(forecast);
+        return this.filterForecast3(forecast);
     },
 
     filterCurrentWeather: function (currentWeather, weatherResponse) {
@@ -65,13 +65,13 @@ var openweathermap = {
         let weatherValue = {};
         for (let i in forecast.list) {
             let date = new Date(forecast.list[i].dt*1000);
-            if (date.getHours() == 12) {
+            if (date.getHours() < 12) {
                 let key = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
                 weatherValue = this.getWeatherData(forecast.list[i]);
                 values = key in data ? data[key] : {};
                 values.day = weatherValue;
                 data[key] = values;
-            } else if (date.getHours() == 0) {
+            } else if (date.getHours() >= 12) {
                 // Le resto uno porque la hora 0:00 la toma como si fuera el día siguiente
                 let key = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate()-1);
                 weatherValue = this.getWeatherData(forecast.list[i]);
@@ -83,9 +83,82 @@ var openweathermap = {
         return data;
     },
 
-    getWeatherData: function (forecast) {
+    filterForecast2: function (forecast) {
+        let data = {};
+        for (let i in forecast.list) {
+            let keyDate = forecast.list[i].dt_txt.split(' ')[0];
+            let keyHour = forecast.list[i].dt_txt.split(' ')[1].split(':')[0];
+            if (data[keyDate] === undefined) {
+                console.log("AGREGO "+ keyDate)
+                data[keyDate] = [];
+            }
+            console.log("HORA "+ keyHour);
+            //data[keyDate].push(keyHour);
+            data[keyDate].push(this.getWeatherData(forecast.list[i], keyHour));
+        }
+        return data;
+    },
+
+    filterForecast3: function (forecast) {
+        let data = {};
+        let currentDate = undefined;
+        for (let i in forecast.list) {
+            let keyDate = forecast.list[i].dt_txt.split(' ')[0];
+            let keyHour = forecast.list[i].dt_txt.split(' ')[1].split(':')[0];
+
+            if (currentDate === undefined) {
+                currentDate = keyDate;
+                data[currentDate] = {};
+            }
+            if (keyHour < 12) {
+                if(data[currentDate].day === undefined) {
+                    data[currentDate].day = {};
+                }
+                // Tomo el icono de las 9 horas como el representativo de la mañana
+                if (keyHour == 9) {
+                    data[currentDate].day.description = forecast.list[i].weather[0].description;
+                }
+                let temp = data[currentDate].day.temperature === undefined ? 0 : data[currentDate].day.temperature;
+                let count = data[currentDate].day.size === undefined ? 0 : data[currentDate].day.size;
+                temp += (forecast.list[i].main.temp_min + forecast.list[i].main.temp_max) / 2;
+                count++;
+                data[currentDate].day.temperature = temp;
+                data[currentDate].day.size = count;
+            } else {
+                if(data[currentDate].night === undefined) {
+                    data[currentDate].night = {};
+                }
+                // Tomo el icono de las 21 horas con el representativo de la noche
+                if (keyHour == 21) {
+                    data[currentDate].night.description = forecast.list[i].weather[0].description;
+                }
+                let temp = data[currentDate].night.temperature === undefined ? 0 : data[currentDate].night.temperature;
+                let count = data[currentDate].night.size === undefined ? 0 : data[currentDate].night.size;
+                temp += (forecast.list[i].main.temp_min + forecast.list[i].main.temp_max) / 2;
+                count++;
+                data[currentDate].night.temperature = temp;
+                data[currentDate].night.size = count;
+            }
+            if (currentDate !== keyDate || (i == (forecast.list.length - 1))) {
+                if (data[currentDate].day != undefined) {
+                    data[currentDate].day.temperature = (data[currentDate].day.temperature / data[currentDate].day.size).toFixed(2);
+                    delete data[currentDate].day.size;
+                }
+                data[currentDate].night.temperature = (data[currentDate].night.temperature / data[currentDate].night.size).toFixed(2);
+                delete data[currentDate].night.size;
+                if (i != (forecast.list.length - 1)) {
+                    currentDate = keyDate;
+                    data[currentDate] = {};
+                }
+            }
+        }
+        return data;
+    },
+
+    getWeatherData: function (forecast, hour) {
         let weatherValue = {};
-        weatherValue.temperature = (forecast.main.temp_min + forecast.main.temp_min) / 2;
+        weatherValue.hour = hour;
+        weatherValue.temperature = (forecast.main.temp_min + forecast.main.temp_max) / 2;
         weatherValue.weather = forecast.weather[0].description;
         return weatherValue;
     }
